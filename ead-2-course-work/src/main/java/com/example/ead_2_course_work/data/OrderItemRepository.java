56585a9c39d739c0792a6a677ea0c8b780042bad
Sasi <sasidulajayara12@gmail.com>
2024-09-05 19:32:02 +0530

    Delete ead-2-course-work directory

--- ead-2-course-work/src/main/java/com/example/ead_2_course_work/data/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ead_2_course_work.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-
-}
--- ead-2-course-work/src/main/java/com/example/ead_2_course_work/data/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ead_2_course_work.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-    List<Order> findByCustomerId(int customerId);
-}
